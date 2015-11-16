de9d73a5e966dc1a0d638464960e223451cb2b59
clojj <jwin1968@gmail.com>
2015-11-17 00:02:32 +0100

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to circle-db
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/circle_db/core_test.clj
@@ -0,0 +1,7 @@
+(ns circle-db.core-test
+  (:require [clojure.test :refer :all]
+            [circle-db.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
